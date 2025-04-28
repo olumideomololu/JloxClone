0fe93592c81a57cb2a61efc395c38932ea5bed14
olumideomololu <oaomololu@gmail.com>
2025-04-28 16:36:17 +0100

    String literal added

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -71,11 +71,30 @@ class Scanner {
             case '\n':
                 line++;
                 break;
+            case '"': string();
+                break;
 
             default:
                 Lox.error(line, "Unexpected character.");
                 break;
         }
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "unterminated string.");
+            return;
+        }
+
+        advance();
+
+        String value = source.substring(start + 1, current + 1);
+        addToken(STRING, value);
     }
 
     private boolean match(char expected) {
